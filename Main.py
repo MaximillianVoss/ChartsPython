298e309e15559176047dbd35d5d151cf1337d892
Александр <fosswolfgang@hotmail.com>
2019-04-30 13:45:06 +0300

    Initial commit

---
+++ Main.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+
+from matplotlib import pyplot as plt
+
+
+def GetMatrix(filename):
+    matrix = []
+    try:
+        file = open(filename)
+        for line in file:
+            matrix.append([float(x) for x in line.split()])
+    except FileNotFoundError:
+        print("file {0} not found!".format(filename))
+    return matrix
+
+
+def GetChart(matrix, title):
+    mult = 4
+    step = 5
+    start = -10
+    plt.figure()
+    if len(matrix) > 3:
+        for i in range(0, 5):
+            plt.plot(range(0, len(matrix[i]) * mult, mult), matrix[i], label="y={0}".format(start))
+            start += step
+    plt.title(title)
+    plt.legend()
+
+def GetBar(matrix,title):
+    plt.figure()
+    plt.bar(range(256), matrix[0])
+    plt.title(title)
+
+titles = ["Red", "Green", "Blue"]
+for i in range(0, 3):
+    GetChart(GetMatrix("{0}.txt".format(titles[i])), titles[i])
+
+
+#TODO:проверить  считывание файлов
+titles = ["Y", "Cb", "Cr"]
+for i in range(0, 3):
+    GetBar(GetMatrix("{0}.txt".format(titles[i])), titles[i])
+plt.show()
